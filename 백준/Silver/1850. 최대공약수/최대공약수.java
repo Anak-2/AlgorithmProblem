cfd1a93cf540bcd6ca00398d0177c8c7c02d8692
Donghyun Kim <83941092+Anak-2@users.noreply.github.com>
2024-09-11 22:21:48 +0900

    [Silver I] Title: 최대공약수, Time: 252 ms, Memory: 38856 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,32 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long result = gcd(B, A);
+
+        StringBuilder sb = new StringBuilder();
+        while(result > 0) {
+            sb.append("1");
+            result--;
+        }
+
+        System.out.println(sb);
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
