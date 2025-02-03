30a7e2e55e27e53cec52e819309072306dae3367
donghyun <ilovekdh1208@ajou.ac.kr>
2025-02-03 22:22:38 +0900

    clean unuse directories

--- Algorithm(Intellij)/Programmers/src/main/java/com/example/level1/No12926.java
+++
@@ -1,4 +0,0 @@
-package com.example.level1;
-
-public class No12926 {
-}
--- 백준/class 3/17219.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
-add = {}
-for _ in range(n):
-    site, ps = input().split()
-    add[site] = ps
-for _ in range(m):
-    print(add[input().rstrip()])
