b94bf0d2225c0f4dd2056b7a8d9de10d62c137e5
donghyun <ilovekdh1208@ajou.ac.kr>
2025-03-10 22:21:38 +0900

    add solution

---
+++ LeetCode/src/hash_map_set/Find_the_Difference_of_Two_Arrays.java
@@ -0,0 +1,55 @@
+package hash_map_set;
+
+import java.util.*;
+
+public class Find_the_Difference_of_Two_Arrays {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> difference = solution.findDifference(new int[]{1, 2, 3, 3},
+                new int[]{1, 1, 2, 2});
+
+        for (List<Integer> list : difference) {
+            System.out.println(list);
+        }
+    }
+
+    static class Solution {
+        public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+            // Use HashSets to store unique elements from both arrays
+            Set<Integer> set1 = new HashSet<>();
+            Set<Integer> set2 = new HashSet<>();
+
+            // Populate set1 with elements from nums1
+            for (int num : nums1) {
+                set1.add(num);
+            }
+
+            // Populate set2 with elements from nums2
+            for (int num : nums2) {
+                set2.add(num);
+            }
+
+            // Lists to store elements that are unique to each array
+            List<Integer> list1 = new ArrayList<>();
+            List<Integer> list2 = new ArrayList<>();
+
+            // Find elements in set1 that are NOT in set2
+            for (int num : set1) {
+                if (!set2.contains(num)) {
+                    list1.add(num);
+                }
+            }
+
+            // Find elements in set2 that are NOT in set1
+            for (int num : set2) {
+                if (!set1.contains(num)) {
+                    list2.add(num);
+                }
+            }
+
+            // Return the two lists as a single result
+            return Arrays.asList(list1, list2);
+        }
+    }
+}
